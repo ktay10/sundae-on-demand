16c9b08c326c78132c5bbf04e1e76ca3d151d72c
KhaledTaymour <khaled.taymour@gmail.com>
2022-02-05 14:29:01 +0200

    Adding Popver test+code and wait for its disappear

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+import { OverlayTrigger, Popover } from "react-bootstrap";
 
 const SummaryForm = () => {
   const [isChkBxChecked, setIsChkBxChecked] = useState(false);
+
+  const TermsAndConditions = () => (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">T&Cs</Popover.Header>
+      <Popover.Body>I agree</Popover.Body>
+    </Popover>
+  );
 
   const onCheck = (e) => {
     setIsChkBxChecked((prev) => !prev);
@@ -9,10 +17,12 @@ const SummaryForm = () => {
 
   return (
     <div>
-      <label>
-        <input type={"checkbox"} onChange={onCheck}></input>
-        {"Terms and Conditions"}
-      </label>
+      <OverlayTrigger trigger={"hover"} overlay={TermsAndConditions}>
+        <label>
+          <input type={"checkbox"} onChange={onCheck}></input>
+          {"Terms and Conditions"}
+        </label>
+      </OverlayTrigger>
       <button disabled={!isChkBxChecked}>{`Confirm Order`}</button>
     </div>
   );
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,9 +1,12 @@
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 describe("chkBx interaction with the btn", () => {
-  afterEach(cleanup); // clean up the DOM after each test
-
   test("chkbx should disabled by defaut and btn is disabled", () => {
     render(<SummaryForm />);
     const chkBx = screen.getByRole("checkbox", {
@@ -22,10 +25,25 @@ describe("chkBx interaction with the btn", () => {
     });
     const btn = screen.getByRole("button", { name: /confirm order/i });
 
-    fireEvent.click(chkBx);
+    userEvent.click(chkBx);
     expect(btn).toBeEnabled();
 
-    fireEvent.click(chkBx);
+    userEvent.click(chkBx);
     expect(btn).toBeDisabled();
+  });
+
+  test("popover appears when hovering on the label. Then unhover", async () => {
+    render(<SummaryForm />);
+    const nullPopover = screen.queryByText(/I agree/i);
+    expect(nullPopover).not.toBeInTheDocument();
+
+    const lbl = screen.getByText(/terms and conditions/i);
+    userEvent.hover(lbl);
+    const popover = screen.getByText(/I agree/i);
+    expect(popover).toBeInTheDocument();
+
+    userEvent.unhover(lbl);
+    // use waitForElementToBeRemoved to wait for async actions as in this case of disappearing the Popver
+    await waitForElementToBeRemoved(() => screen.queryByText(/I agree/i));
   });
 });
